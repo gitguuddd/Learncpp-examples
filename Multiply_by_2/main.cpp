7f9e60030be527b8fe86fe504a026da06faa0f4b
Mindaugas Kasiulis <mkasiulis49@gmail.com>
2019-07-16 18:51:10 +0300

    Multiply by 2 example

---
+++ Multiply_by_2/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout <<"Enter an integer: ";
+    int num{};//always initialize your variables :)
+    std::cin>>num;
+    std::cout<<"Double "<<num<<"is "<<num*2<<'\n';//we need to return double the input
+    std::cout<<"Triple "<<num<<"is "<<num*3<<'\n';//we need to return triple the input
+    return 0;
+}
