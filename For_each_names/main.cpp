85cba93ee24d8002df891a5b406b6a9dd1c1f410
Mindaugas Kasiulis <mkasiulis49@gmail.com>
2019-07-19 23:17:52 +0300

    For each loop in searching for names in an array

---
+++ For_each_names/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string array[]{"Alex", "Betty", "Caroline", "Dave", "Emily", "Fred", "Greg", "Holly"};
+    std::cout<<" Please enter a name to be used in the search: ";
+    std::string name{};
+    std::cin>>name;
+    bool contains(false);
+    for(auto const& names:array){
+        if(names==name)
+            contains=true;}
+    if(contains)
+        std::cout<<name<<" was found.\n";
+    else
+        std::cout<<name<<" was not found.\n";
+
+    return 0;
+}
